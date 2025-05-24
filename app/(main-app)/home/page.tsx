451827e4d26360728987e6f09831563a2b80c574
Rishabh Dotasara <rishabhdotasara@gmail.com>
2025-05-24 11:51:21 +0530

    patch

--- app/(main-app)/home/page.tsx
+++ app/(main-app)/home/page.tsx
@@ -1,8 +1,13 @@
-
 import Home from "@/components/custom/home/home";
 import withAuth from "@/hoc/auth";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 function HomePage() {
-  return <Home />;
+  return (
+    <Suspense fallback={<div className="h-screen w-full flex items-center justify-center"><Loader2 className="animate-spin"/></div>}>
+      <Home />
+    </Suspense>
+  );
 }
 
 export default HomePage;
